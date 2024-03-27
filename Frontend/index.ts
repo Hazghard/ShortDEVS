b7fff38622e0d459041a9e0087a2cbcaf75c35e2
Florimond <lapiflo45@gmail.com>
2024-03-27 13:34:53 +0100

    first commit

--- Frontend/index.ts
+++ Frontend/index.ts
@@ -3,29 +3,27 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    await prisma.post.update({
-      where: {
-        slug: 'my-first-post',
-      },
-      data: {
-        comments: {
-          createMany: {
-            data: [
-              { comment: 'Great post!' },
-              { comment: "Can't wait to read more!" },
-            ],
+    await prisma.user.create({
+        data: {
+          name: 'Rich',
+          email: 'hello@prisma.com',
+          posts: {
+            create: {
+              title: 'My first post',
+              body: 'Lots of really interesting stuff',
+              slug: 'my-first-post',
+            },
           },
         },
-      },
-    })
-    const posts = await prisma.post.findMany({
-      include: {
-        comments: true,
-      },
-    })
-  
-    console.dir(posts, { depth: Infinity })
-  }
+      })
+    
+      const allUsers = await prisma.user.findMany({
+        include: {
+          posts: true,
+        },
+      })
+      console.dir(allUsers, { depth: null })
+}
 
 main()
   .catch(async (e) => {
